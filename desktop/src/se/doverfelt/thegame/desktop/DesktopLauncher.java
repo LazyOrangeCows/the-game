6238db001c271c6c14cab3773f478bcc831139a7
Rickard Doverfelt <rickard.doverfelt@gmail.com>
2016-09-28 12:02:47 +0200

    Added Files

---
+++ desktop/src/se/doverfelt/thegame/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package se.doverfelt.thegame.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import se.doverfelt.thegame.TheGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new TheGame(), config);
+	}
+}
