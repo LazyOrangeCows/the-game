064013603e0bfbc72db6b458cf2c2838003172ce
Nibor9198 <robin.boregrim@gmail.com>
2016-09-30 18:37:41 +0200

    Glömde adda xD

---
+++ core/src/se/doverfelt/thegame/Screens/LoadingScreen.java
@@ -0,0 +1,59 @@
+package se.doverfelt.thegame.Screens;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+/**
+ * Created by Robin on 2016-09-30.
+ */
+public class LoadingScreen implements Screen{
+    SpriteBatch batch;
+    Texture img;
+
+    @Override
+    public void show() {
+        System.out.println("Hej");
+        batch = new SpriteBatch();
+        img = new Texture("badlogic.jpg");
+    }
+
+    @Override
+    public void render(float delta) {
+        Gdx.gl.glClearColor(1, 0, 0, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        batch.begin();
+        batch.draw(img, 0, 0);
+        batch.end();
+    }
+    public void update(float delta){
+
+    }
+
+    @Override
+    public void resize(int width, int height) {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+}
