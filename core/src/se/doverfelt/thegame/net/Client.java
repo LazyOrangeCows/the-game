2ec559b13dedd9ea9ed631cb7faffa9ab5664393
Rickard Doverfelt <rickard.doverfelt@gmail.com>
2017-01-10 10:40:58 +0100

    Fixed scaling ping interval

--- core/src/se/doverfelt/thegame/net/Client.java
+++ core/src/se/doverfelt/thegame/net/Client.java
@@ -74,7 +74,7 @@ public class Client {
                         e.printStackTrace();
                     }
                     try {
-                        Thread.sleep(500);
+                        Thread.sleep((long)(Math.max(1000 * (Math.max(ping, 1)/100f), 500)));
                     } catch (InterruptedException e) {
                         e.printStackTrace();
                     }
